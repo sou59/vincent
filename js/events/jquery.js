6109c7513965cb5094dd1888871c216cab8b5143
Sou59 <sz5962@gmail.com>
2018-11-28 16:08:43 +0100

    addFunction

--- js/events/jquery.js
+++ js/events/jquery.js
@@ -3,12 +3,73 @@
 // scope de la fonction locale, fonction isole, car si autre bibliothèque dans le même js pas de conflit
 // création d'un objet jquery
     const myJquery = {
+        // tableau d'élément, d'objet que je vais récupérer (ex. h1, div...)
+        elements: [],
         // paramètre fonction callback : fnc de rappel
         ready: function (callback) {
+
+            //si le dom est chargé
             document.addEventListener('DOMContentLoaded', callback, false);
-            //si le dom est chargé 
+             
+        },
+
+        /*
+        find: function () {
+            // récupère les paramètres de la fonction
+            // quand on ne connait pas les arguments à l'avance
+            console.log(arguments);
+        }
+        */
+
+        find: function (selectors) {
+            this.elements.push(document.querySelector(selectors));
+            return this;
+        },
+
+        findAll: function (selectors) {
+            const elems = document.querySelectorAll(selectors);
+
+            for (let elem of elems) {
+                this.elements.push(elem);
+            }
+            return this;
+        },
+
+        addClass: function (names) {
+            // voir le foction programming (tout est fonction)
+            this.elements.forEach(function (elem) {
+                // ajoute une class même si il en existe une (attention normalement d'autre )
+                // s'il y a une class existante ajouter un espace avant, sinon pas d'espace
+
+                // en ternaire
+                elem.className = elem.className ? ' ' + names : names;
+
+                /*
+                if normal : 
+
+                if(elem.className.length > 0) {
+                    elem.className +=  " " + names;
+                } else {
+                    elem.className +=  names;
+                }
+                */
+                
+            });
+            // avec return this permet de chainer à la suite
+            return this;
+        },
+
+        // ajouter un évènements sur l'élément
+        on: function (event, callback) {
+
+            for (let elem of this.elements) {
+                elem.addEventListener(event, callback);
+            }
+            return this;
         }
     }
+
+
 // objet window qui est global
 // tout est private ici, sauf quand on le met en globale par l'intermédiaire de window
 // écrire windows en minuscule
--- js/events/main.js
+++ js/events/main.js
@@ -1,4 +1,22 @@
 // appel de la fonction créer avec jquery.js
+// on crée notre propre jquery
+
 $.ready(function () {
     console.log('DOM loaded');
+
+    // pour l'affichage des arguments dans la console (décommenter ici et ds jquery.js)
+   // $.find("Toto", "Jena", "Xena", "Chuck");
+
+    
+   // $.find("ul").findAll("li").addClass('toto');  //ajoute une class test à tous les éléments
+ 
+    //console.log($.elements);
+    // avec un tableau d'élements on peut leur faire tout ce qu'on veut
+
+    $.findAll('li').on('click', function () {
+       // $.addClass('toto');
+
+       alert("C'est moi TOTO!!");
+    });
+
 });
