b02e5b853ba021de72bfd8ec84949b420c66c84b
Sou59 <sz5962@gmail.com>
2018-11-29 22:28:48 +0100

    add main.js

--- js/jquery/main.js
+++ js/jquery/main.js
@@ -2,15 +2,20 @@ $(document).ready(function () {
 
     $('li').css('color', 'red');
     
-    $val = $('input').val();
-    console.log($val);
-    
-    $('button').click(function(event) {
-        event.preventDefault();
+    // keyup à chaque lettre remplie le p
+    var valinput = $('#userinput').keyup(function() {
+        // récupération de la valeur tapé dans l'input
+        var text = valinput.val();
 
-        $('p').text($val);
-
+        //insertion dans le p.insert
+        $(".insert").text(text);
     });
 
+/*
+    $( "#submit" ).click(function() {
+        var text = $("#userinput").val();
 
+        $(".insert").text(text);
+    });
+*/
 });
